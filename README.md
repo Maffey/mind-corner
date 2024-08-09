2a60427b7130c11c9c91538d7e80477cb28395b8
Mateusz Kuniczuk <mateusz.kuniczuk98@gmail.com>
2024-08-09 22:44:59 +0200

    groundwork

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mind Corner
+
+Based on saved idea from a long time ago.
+The application should be a simple (for now) CLI corner for basic mental health activities:
+1. Meditation Timer
+2. Gratitiude Journal
+3. Mood Tracker
+4. Breathing Guide
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+
+/// Based on saved idea from a long time ago.
+/// The application should be a simple (for now) CLI corner for basic mental health activities:
+/// 1. Meditation Timer
+/// 2. Gratitiude Journal
+/// 3. Mood Tracker
+/// 4. Breathing Guide
+pub fn main() {
     println!("Hello, world!");
+    // TODO Implement inquire (#1)
 }
