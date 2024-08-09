cf404ea942a98ffb35189f4ccf0c61906aeb5a60
Mateusz Kuniczuk <mateuszkuniczuk98@gmail.com>
2024-08-09 23:58:36 +0200

    User input added

--- src/app_modules/meditation_timer.rs
+++ src/app_modules/meditation_timer.rs
@@ -1,16 +1,27 @@
 use std::io::{stdout, Write};
 use std::thread::sleep;
 use std::time::Duration;
+use inquire::Text;
 
 const SECONDS_IN_MINUTE: u32 = 60;
 
 pub fn run_meditation_timer() {
-    // TODO Take time input
-    let meditation_time: u32 = 5;
+    // TODO Reattempt reading on failure
+    let duration: String = Text::new("Enter the duration of your meditation in minutes:")
+        .prompt()
+        .expect("Failed to read duration time.");
 
+    let duration: u32 = duration.trim().parse().expect("Please enter a valid number.");
+
+    start_timer(duration * SECONDS_IN_MINUTE);
+}
+
+
+pub fn start_timer(duration: u32) {
+    println!("Starting meditation timer.");
     let mut standard_output = stdout();
 
-    for seconds in 0..=meditation_time {
+    for seconds in 0..=duration {
         let minutes = seconds / SECONDS_IN_MINUTE;
         let seconds_in_minute = seconds % SECONDS_IN_MINUTE;
 
