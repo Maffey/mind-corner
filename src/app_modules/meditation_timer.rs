62bb6def305740733dd2fec0644aaf4702e205a7
Mateusz Kuniczuk <mateuszkuniczuk98@gmail.com>
2024-10-09 20:08:23 +0200

    add validation to meditation timer

--- src/app_modules/meditation_timer.rs
+++ src/app_modules/meditation_timer.rs
@@ -1,4 +1,5 @@
 use crate::data_analysis::meditation_timer::add_record;
+use inquire::validator::Validation;
 use inquire::Text;
 use log::{error, info};
 use std::io::{stdout, Write};
@@ -6,21 +7,29 @@ use std::thread::sleep;
 use std::time::Duration;
 
 const SECONDS_IN_MINUTE: u32 = 60;
+const DEFAULT_MINUTES_TIMER: u32 = 5;
 
 pub fn run_meditation_timer() {
-    // TODO Add inquire here to use Select, timer in 10 or 30 second intervals (#1)
+    let is_number_validator = |input: &str| match input.chars().all(|c| c.is_numeric()) {
+        true => Ok(Validation::Valid),
+        false => Ok(Validation::Invalid(
+            "Please enter a valid natural number.".into(),
+        )),
+    };
+
     let duration: String = Text::new("Enter the duration of your meditation in minutes:")
+        .with_default(&DEFAULT_MINUTES_TIMER.to_string())
+        .with_validator(is_number_validator)
         .prompt()
         .expect("Failed to read duration time.");
 
     let duration: u32 = duration
         .trim()
         .parse()
-        .expect("Please enter a valid number.");
+        .unwrap_or_else(|_| DEFAULT_MINUTES_TIMER);
 
     start_timer(duration * SECONDS_IN_MINUTE);
 
-    // TODO Add generic retry mechanism
     // TODO Analyze data with polars (#2)
     info!("Adding record to local CSV file...");
     match add_record(duration) {
@@ -32,7 +41,7 @@ pub fn run_meditation_timer() {
     }
 }
 
-pub fn start_timer(duration: u32) {
+fn start_timer(duration: u32) {
     println!("Starting meditation timer.");
     let mut standard_output = stdout();
 
