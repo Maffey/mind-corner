5ea70c3acf33424ea24028757830b849e2e05db7
Mateusz Kuniczuk <mateuszkuniczuk98@gmail.com>
2024-10-09 16:11:24 +0200

    notes for future

--- src/app_modules/mod.rs
+++ src/app_modules/mod.rs
@@ -1,5 +1,13 @@
 mod meditation_timer;
 
 pub fn select_module() {
+    // let options: Vec<&str> = vec!["Banana", "Apple", "Strawberry", "Grapes", "Lemon", "Tangerine", "Watermelon", "Orange", "Pear", "Avocado", "Pineapple"];
+    // 
+    // let ans: Result<&str, InquireError> = Select::new("What's your favorite fruit?", options).prompt();
+    // 
+    // match ans {
+    //     Ok(choice) => println!("{}! That's mine too!", choice),
+    //     Err(_) => println!("There was an error, please try again"),
+    // }
     meditation_timer::run_meditation_timer();
 }
