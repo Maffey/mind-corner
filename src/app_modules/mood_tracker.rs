797ace0b59034dfa124e2d7f6bee3628c76a7492
Mateusz Kuniczuk <mateuszkuniczuk98@gmail.com>
2025-01-28 22:14:40 +0100

    Add mood selection with emojis and implement Mood struct
    Replaced basic mood selection with a structured `Mood` type including ratings and emojis. Integrated a custom `fmt::Display` implementation to improve display formatting and enhanced user experience by adding help messages and default cursor positioning.

--- src/app_modules/mood_tracker.rs
+++ src/app_modules/mood_tracker.rs
@@ -1,13 +1,40 @@
-use inquire::{InquireError, Select};
+use std::fmt;
+use inquire::Select;
+use inquire::error::InquireResult;
 
 pub(crate) fn run_mood_tracker() {
-    println!("WIP");
     // TODO This should be a simple 1-5 selector at the beginning (ideally with emojis) saved to CSV file.
     // TODO later, consider SQLite and more options.
     // TODO Idea: user is pestered with questions if empty line is sent then the program stops early
-    let mood_options = vec!["😭","😔", "😐", "😊", "😄"];
-    let user_mood: Result<&str, InquireError> =
-        Select::new("How would you rate your mood?", mood_options).prompt();
+    
+    let mood_options = vec![
+        Mood { emoji: "😭", rating: 1 },
+        Mood { emoji: "😔", rating: 2 },
+        Mood { emoji: "😐", rating: 3 },
+        Mood { emoji: "😊", rating: 4 },
+        Mood { emoji: "😄", rating: 5 },
+    ];
+    
+    let user_mood: InquireResult<Mood> =
+        Select::new("How would you rate your mood?\n", mood_options)
+            .with_help_message("↑↓ to move, enter to select")
+            .with_starting_cursor(2)
+            .without_filtering()
+            .prompt();
+    
     println!("{:?}", user_mood);
 
+}
+
+
+#[derive(Debug)]
+struct Mood {
+    emoji: &'static str,
+    rating: u8,
+}
+
+impl fmt::Display for Mood {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ({})", self.emoji, self.rating)
+    }
 }
