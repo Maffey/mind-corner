891b27a2437155a80dc6019cc714cd787a6073be
Mateusz Kuniczuk <mateusz.kuniczuk98@gmail.com>
2025-02-04 20:13:02 +0100

    plans

--- src/app_modules/gratitude_journal.rs
+++ src/app_modules/gratitude_journal.rs
@@ -4,12 +4,6 @@ use inquire::{required, Editor};
 use rand::seq::SliceRandom;
 
 pub(crate) fn run_gratitude_journal() {
-    // TODO Follow the same flow for now - store data into a csv file(??)
-    //  Ask a nice question "What are you grateful for today?" Have 5 random qusetions like that.
-    // TODO Consider following possibility:
-    //  making multiple entries a day should not add new ones with a timestamp,
-    //  but instead, pop-up the previously created one so the user can edit it. Something like editing git commit message:
-    //  https://stackoverflow.com/questions/56011927/how-do-i-use-rust-to-open-the-users-default-editor-and-get-the-edited-content
 
     // TODO inquire::Editor?
     //  If the user presses enter without ever modyfing the temporary file, it will be treated as an empty submission.
@@ -21,12 +15,19 @@ pub(crate) fn run_gratitude_journal() {
 
     let journal_entry_header: &str = &format!("# Journal Entry - {}", get_date());
 
-    let journal_entry = Editor::new(prompt)
+    // TODO Consider following possibility:
+    //  making multiple entries a day should not add new ones with a date,
+    //  but instead, pop-up the previously created one so the user can edit it. Something like editing git commit message:
+    //  https://stackoverflow.com/questions/56011927/how-do-i-use-rust-to-open-the-users-default-editor-and-get-the-edited-content
+
+    let journal_entry: String = Editor::new(prompt)
         .with_predefined_text(journal_entry_header)
         .with_file_extension(".md")
+        // TODO validator to ensure default header is checked too
         .with_validator(required!())
         .prompt()
         .expect("Failed to get journal entry.");
 
     println!("{}", journal_entry);
+    // TODO store to a text file in subdirectory, instead of csv.
 }
