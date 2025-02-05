390cbacce78eee4edaa5637f2aba5593c45132ae
Mateusz Kuniczuk <mateusz.kuniczuk98@gmail.com>
2025-02-05 19:00:02 +0100

    drop todo as doc

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,6 @@ mod data;
 mod project_consts;
 mod utilities;
 
-/// Based on saved idea from a long time ago.
-/// The application should be a simple CLI corner for basic mental health activities:
-/// 1. Meditation Timer
-/// 2. Gratitiude Journal
-/// 3. Mood Tracker
-/// 4. Breathing Guide
-/// 5. Data Analysis
 pub fn main() {
     env_logger::init();
     println!("Welcome to Mind Corner!");
