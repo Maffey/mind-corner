b8d70784dbc19e6ace89a1185b9a4a0d88d315e4
Maffey <mateusz.kuniczuk98@gmail.com>
2025-02-08 18:50:44 +0100

    unwraps dropped

--- src/data/data_collection.rs
+++ src/data/data_collection.rs
@@ -3,5 +3,5 @@ use std::io;
 use std::io::Seek;
 
 pub(crate) fn does_file_need_headers(csv_file: &mut File) -> bool {
-    csv_file.seek(io::SeekFrom::End(0)).unwrap() == 0
+    csv_file.seek(io::SeekFrom::End(0)).expect("Failed to read CSV file length.") == 0
 }
